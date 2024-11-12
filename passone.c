47b935608dc161229423750ba6dbbcee34ef16a3
Manu Emmanuel <manuemmanuel228@gmail.com>
2024-11-12 20:31:57 +0530

    file pointers

--- passone.c
+++ passone.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
+void main(){
+    FILE *optab, *source, *intermediate, *symtab, *prgmlength;
+    source = fopen("source.txt","r");
+    optab = fopen("optab.txt", "r");
+    intermediate = fopen("intermediate.txt", "w");
+    symtab = fopen("symtab.txt", "w+");
+    prgmlength = fopen("length.txt", "w");
+
+    int label[20], opcode[20], operand[20], symtab_data, optab_data;
+}
